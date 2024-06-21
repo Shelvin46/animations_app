bac0f9175f3ff9afdfab5284b674413d2d5be748
Shelvin46 <shelvinvarghese6@gmail.com>
2024-06-21 12:38:22 +0530

    chore: Update animation screen title and layout

--- lib/features/animation/presentation/screens/animation_screen.dart
+++ lib/features/animation/presentation/screens/animation_screen.dart
@@ -11,9 +11,44 @@ class _TweenAnimationScreenState extends State<TweenAnimationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Tween Animation'),
+      appBar: AppBar(
+        title: const Text('Animations'),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TweenAnimationBuilder(
+                curve: Curves.bounceOut,
+                onEnd: () {
+                  debugPrint('Tween Animation Ended');
+                },
+                tween: Tween<double>(begin: 0, end: 100),
+                duration: const Duration(seconds: 2),
+                builder: (context, size, child) {
+                  return Column(
+                    children: [
+                      Icon(
+                        Icons.favorite,
+                        size: size,
+                        color: Colors.red,
+                      ),
+                      Text(
+                        'Flutter Tween Animation',
+                        style: TextStyle(
+                          fontSize: size / 2,
+                          color: Colors.blue,
+                        ),
+                      )
+                    ],
+                  );
+                },
+              ),
+            ],
+          ),
         ),
-        body: const SafeArea(child: Column()));
+      ),
+    );
   }
 }
